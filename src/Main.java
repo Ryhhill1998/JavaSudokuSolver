7298063edc0592f22aef6b31b8b7e9c1abf30560
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2022-11-17 10:58:41 +0000

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,23 @@ public class Main {
     public static void main(String[] args) {
 //        int[][] gameboard = getGameBoardFromInput();
 
-        int[][] gameboard = {{1,0,3,0},{0,4,2,1},{0,0,0,2},{0,0,4,0}};
+//        int[][] gameboard = {
+//                {1,0,6,0,7,8,0,3,0},{0,5,7,3,4,9,0,6,2},{2,9,3,6,0,5,0,0,7},
+//                {0,0,0,8,0,1,6,0,4},{4,6,1,0,0,0,2,8,9},{5,8,0,0,6,4,3,0,0},
+//                {7,3,4,1,9,6,5,0,8},{6,1,8,7,0,2,4,9,3},{0,0,0,0,8,0,0,1,0}
+//        };
+
+        int[][] gameboard = {
+                {0,0,4,1,0,0,0,0,0},{0,0,0,2,9,0,3,0,1},{0,9,0,0,7,0,8,6,0},
+                {1,3,0,9,0,7,5,8,2},{9,4,8,5,0,6,1,0,7},{0,0,2,8,0,3,0,0,0},
+                {6,1,0,7,0,0,0,0,0},{4,0,7,0,0,0,2,1,0},{0,0,0,0,8,0,0,0,6}
+        };
 
         SudokuSolver sudokuSolver = new SudokuSolver(gameboard);
 
         sudokuSolver.printGameboard();
         System.out.println();
-
-        sudokuSolver.printColumns();
-        System.out.println();
-
-        sudokuSolver.solveForNumber(1);
-        System.out.println();
-        sudokuSolver.solveForNumber(2);
-        System.out.println();
-        sudokuSolver.solveForNumber(3);
-        System.out.println();
-        sudokuSolver.solveForNumber(4);
+        sudokuSolver.solvePuzzle();
     }
 
     public static int[][] getGameBoardFromInput() {
--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -6,11 +6,20 @@ public class SudokuSolver {
 
     private int[][] gameboard;
     private int[][] columns;
+    private int[][] gridRow0;
+    private int[][] gridRow1;
+    private int[][] gridRow2;
+    private int spacesLeft = 0;
 
     public SudokuSolver(int[][] gameboard) {
         this.gameboard = gameboard;
         columns = new int[gameboard.length][gameboard.length];
         setColumns();
+        gridRow0 = new int[3][9];
+        gridRow1 = new int[3][9];
+        gridRow2 = new int[3][9];
+        setGrids();
+        setSpacesLeft();
     }
 
     private void setColumns() {
@@ -25,9 +34,71 @@ public class SudokuSolver {
         }
     }
 
+    private void setGrids() {
+        setGridRow0();
+        setGridRow1();
+        setGridRow2();
+    }
+
+    private void setGridRow0() {
+        for (int n = 0; n < 3; n++) {
+            int column = n * 3;
+            int[] grid = new int[9];
+            int position = 0;
+
+            for (int row = 0; row < 3; row++) {
+                for (int i = column; i < column + 3; i++) {
+                    grid[position] = gameboard[row][i];
+                    position++;
+                }
+            }
+
+            gridRow0[n] = grid;
+        }
+    }
+    private void setGridRow1() {
+        for (int n = 0; n < 3; n++) {
+            int column = n * 3;
+            int[] grid = new int[9];
+            int position = 0;
+
+            for (int row = 3; row < 6; row++) {
+                for (int i = column; i < column + 3; i++) {
+                    grid[position] = gameboard[row][i];
+                    position++;
+                }
+            }
+
+            gridRow1[n] = grid;
+        }
+    }
+
+    private void setGridRow2() {
+        for (int n = 0; n < 3; n++) {
+            int column = n * 3;
+            int[] grid = new int[9];
+            int position = 0;
+
+            for (int row = 6; row < 9; row++) {
+                for (int i = column; i < column + 3; i++) {
+                    grid[position] = gameboard[row][i];
+                    position++;
+                }
+            }
+
+            gridRow2[n] = grid;
+        }
+    }
+
+    public void printGrids() {
+        for (int[] grid : gridRow0) System.out.println(Arrays.toString(grid));
+        for (int[] grid : gridRow1) System.out.println(Arrays.toString(grid));
+        for (int[] grid : gridRow2) System.out.println(Arrays.toString(grid));
+    }
+
     public void printGameboard() {
         for (int i = 0; i < gameboard.length; i++) {
-            System.out.printf("Row %d: %s\n", i + 1, Arrays.toString(gameboard[i]));
+            System.out.printf("Row %d: %s\n", i, Arrays.toString(gameboard[i]));
         }
     }
 
@@ -37,30 +108,51 @@ public class SudokuSolver {
         }
     }
 
-    public void solveForNumber(int number) {
-        int i = 0;
-        ArrayList<ArrayList<Integer>> possiblePositions;
+    private void setSpacesLeft() {
+        for (int[] row : gameboard) {
+            for (int space : row) {
+                if (space == 0) spacesLeft++;
+            }
+        }
+    }
 
-        while (i < 2) {
-            possiblePositions = getPossiblePositions(number);
-            addNumberToBestRow(possiblePositions, number);
-            setColumns();
-            i++;
+    public void solvePuzzle() {
+        int number = 1;
+
+        while (spacesLeft > 0) {
+            if (number == 10) number = 1;
+            solveForNumber(number);
+            number++;
         }
 
         printGameboard();
     }
 
-    public void addNumberToBestRow(ArrayList<ArrayList<Integer>> possiblePositions, int number) {
+    public void solveForNumber(int number) {
+        boolean added = true;
+        ArrayList<ArrayList<Integer>> possiblePositions;
+
+        while (added) {
+            possiblePositions = getPossiblePositions(number);
+            added = addNumberToBestRow(possiblePositions, number);
+            setColumns();
+        }
+    }
+
+    public boolean addNumberToBestRow(ArrayList<ArrayList<Integer>> possiblePositions, int number) {
+        HashSet<Integer> rowsSeen = new HashSet<>();
         HashSet<Integer> uniqueRows = new HashSet<>();
 
         for (ArrayList<Integer> position : possiblePositions) {
             int row = position.get(0);
-            if (uniqueRows.contains(row)) {
+
+            if (rowsSeen.contains(row)) {
                 uniqueRows.remove(row);
             } else {
                 uniqueRows.add(row);
             }
+
+            rowsSeen.add(row);
         }
 
         for (ArrayList<Integer> position : possiblePositions) {
@@ -68,28 +160,35 @@ public class SudokuSolver {
             if (!uniqueRows.contains(row)) continue;
             int column = position.get(1);
             gameboard[row][column] = number;
+            spacesLeft--;
+//            System.out.printf("%d added to row: %d, col: %d\n", number, row, column);
+            return true;
         }
+
+        return false;
     }
-//
-//    public void addNumberToBestColumn(ArrayList<ArrayList<Integer>> possiblePositions, int number) {
-//        HashSet<Integer> uniqueColumns = new HashSet<>();
-//
-//        for (ArrayList<Integer> position : possiblePositions) {
-//            int column = position.get(1);
-//            if (uniqueColumns.contains(column)) {
-//                uniqueColumns.remove(column);
-//            } else {
-//                uniqueColumns.add(column);
-//            }
-//        }
-//
-//        for (ArrayList<Integer> position : possiblePositions) {
-//            int column = position.get(1);
-//            if (!uniqueColumns.contains(column)) continue;
-//            int row = position.get(0);
-//            gameboard[row][column] = number;
-//        }
-//    }
+
+    public boolean positionGridContainsNumber(ArrayList<Integer> position, int number) {
+        int row = position.get(0);
+        int column = position.get(1);
+
+        int gridRowNumber = row / 3;
+        int[][] gridRow = gridRow0;
+
+        switch (gridRowNumber) {
+            case 1:
+                gridRow = gridRow1;
+                break;
+            case 2:
+                gridRow = gridRow2;
+                break;
+        }
+
+        int gridIndex = column / 3;
+        int[] grid = gridRow[gridIndex];
+
+        return sectionContainsElement(grid, number);
+    }
 
     public ArrayList<ArrayList<Integer>> getPossiblePositions(int number) {
         ArrayList<ArrayList<Integer>> possiblePositions = new ArrayList<>();
@@ -105,7 +204,7 @@ public class SudokuSolver {
                 ArrayList<Integer> position = new ArrayList<>();
                 position.add(i);
                 position.add(j);
-                possiblePositions.add(position);
+                if (!positionGridContainsNumber(position, number)) possiblePositions.add(position);
             }
         }
 
